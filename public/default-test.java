23ccd202b10ce931ff797e656fe81728a1588dc6
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2020-04-09 09:24:10 +0900

    more font size, use tab in ace editor, better default test

--- public/default-test.java
+++ public/default-test.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CloseToZeroTest {
   @Test
   public void test01() {
-    assertEquals(9, CloseToZero.run(10));
+    assertEquals(4, CloseToZero.run(5));
   }
 
   @Test
@@ -16,7 +16,7 @@ public class CloseToZeroTest {
 
   @Test
   public void test03() {
-    assertEquals(-9, CloseToZero.run(-10));
+    assertEquals(-4, CloseToZero.run(-5));
   }
 
 }
