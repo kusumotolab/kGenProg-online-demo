ee655ba5b8e6d2768123f94f49f21f83ff08f910
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2020-04-22 15:46:16 +0900

    add comment for deafult-src

--- public/default-src.java
+++ public/default-src.java
@@ -2,9 +2,10 @@ package example;
 
 public class CloseToZero {
 
+  /* Increase or reduce the given value to be close to zero. */
   public static int run(int n) {
     if (n == 0) {
-      n++; // bug here
+      n++; // a bug here. zero should be zero
     } else if (n > 0) {
       n--;
     } else {
