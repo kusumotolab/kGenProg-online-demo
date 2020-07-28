b411e4dd1491ccd648b10f498ed7c11d9c91988a
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2020-07-27 23:30:14 -0400

    refactor build.sh; enable log, specify container name as kdemo, add comments

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-git pull
+# enable DCT
+export DOCKER_CONTENT_TRUST=1
 
-sudo docker build -t kdemo .
-sudo docker run -it -p 80:3000 kdemo
+sudo docker build -t kdemo-image .
+
+sudo docker run \
+     -p 80:3000              `# expose local 3000 as global 80` \
+     --rm                    `# immediately remove container when stopped` \
+     -d                      `# run as detach mode to be background` \
+     --log-driver=json-file  `# keep logs` \
+     --log-opt max-size=10m  `# keep logs` \
+     --log-opt max-file=100  `# keep logs` \
+     --name kdemo             \
+     kdemo-image              \
