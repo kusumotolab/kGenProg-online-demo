c2cfbb6c2d0cd8248651593a7c4b4452654d9ecc
t-kuma <tts.stt.645@gmail.com>
2020-08-11 15:08:49 +0900

    change submission directory name

--- routes/submission.js
+++ routes/submission.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs').promises;
-const submissionBase = path.resolve('./tmp/kdemo');
+const submissionBase = path.resolve('./tmp/submission');
 
 async function deploy(file, data) {
   await fs.mkdir(path.dirname(file), {recursive: true});
