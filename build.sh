155f35309b708e94bf99481edd57194828e4a597
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2022-04-25 09:17:32 +0000

    workaround for unexpected terminate using --restart on docker-run command

--- build.sh
+++ build.sh
@@ -6,11 +6,13 @@ export DOCKER_CONTENT_TRUST=1
 sudo docker build -t kdemo-image .
 
 sudo docker run \
-     -p 3000:3000            `# expose local 3000` \
-     --rm                    `# immediately remove container when stopped` \
-     -d                      `# run as detach mode to be background` \
-     --log-driver=json-file  `# keep logs` \
-     --log-opt max-size=10m  `# keep logs` \
-     --log-opt max-file=100  `# keep logs` \
+     -p 3000:3000             `# expose local 3000` \
+     -d                       `# run as detach mode to be background` \
+     --log-driver=json-file   `# keep logs` \
+     --log-opt max-size=10m   `# keep logs` \
+     --log-opt max-file=100   `# keep logs` \
+     --restart unless-stopped `# restart always` \
      --name kdemo             \
      kdemo-image              \
+
+#    --rm                     `# immediately remove container when stopped` \
